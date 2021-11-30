5d324050098c2efb4354650ce649daf5f38c290f
gobelinmalin <mariemeline.paus@gmail.com>
2021-11-30 16:02:53 +0100

    mise a jout bcrypt'

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const connection = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 router.post("/login", (req, res) => {
