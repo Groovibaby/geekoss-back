4556e7133e6bcdee1898224a2f6a6b9b1bdb3373
Thomas <thomas.lachieze@gmail.com>
2022-01-09 22:44:42 +0100

    change db.js to reconnect service when missing requests

--- db.js
+++ db.js
@@ -3,11 +3,61 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// module.exports = connection;
+
+// const mysql = require("mysql2");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+const pool = mysql.createPool({
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  // acquireTimeout: 60 * 60 * 1000,
+  // timeout: 60 * 60 * 1000,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  insecureAuth: true,
 });
 
-module.exports = connection;
+module.exports = {
+  // twilio: {
+  //   accountSid: process.env.TWILIO_ACCOUNT_SID,
+  //   apiKey: process.env.TWILIO_API_KEY,
+  //   apiSecret: process.env.TWILIO_API_SECRET,
+  // },
+  query: function () {
+    var sql_args = [];
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+      args.push(arguments[i]);
+    }
+    var callback = args[args.length - 1]; //last arg is callback
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        console.log(err);
+        return callback(err);
+      }
+      if (args.length > 2) {
+        sql_args = args[1];
+      }
+      connection.query(args[0], sql_args, function (err, results) {
+        connection.release(); // always put connection back in pool after last query
+        if (err) {
+          console.log(err);
+          return callback(err);
+        }
+        callback(null, results);
+      });
+    });
+  },
+};
